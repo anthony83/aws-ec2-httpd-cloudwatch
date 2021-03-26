f6a9f25b930369ab4a9f919443b44265549e7b28
anthony83 <nardoneanth83@gmail.com>
2021-03-26 11:23:52 +1100

    updated make script, included flags for create and deleting stacks

--- create-instance-make.sh
+++ create-instance-make.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-templatepath="file:////home/anthonyn/devops/aws/httpd-cloudwatch-logging-ec2/create-instance.yaml"
-region="ap-southeast-2"
-#stackname="stackname"
-keyname="httpd-server"
-sleeptime=45
+templatepath="file:////home/anthonyn/devops/aws/httpd-cloudwatch-logging-ec2/create-instance.yaml" #path to cfn template
+region="ap-southeast-2" 
+keyname="httpd-server" #key name for ssh
 
-read -p "enter stackname " stackname
+#flags logic
+while test $# -gt 0; do
+           case "$1" in
+                -c)
+                    shift
+                    stacknamecreate=$1
+                    aws cloudformation create-stack \
+                    --stack-name $stacknamecreate \
+                    --template-body $templatepath \
+                    --parameters ParameterKey=KeyPairName,ParameterValue=$keyname \
+                    --capabilities CAPABILITY_NAMED_IAM \
+                    --output table
+                    shift
+                    ;;
+                -d)
+                    shift
+                    stacknamedelete=$1
+                    aws cloudformation delete-stack \
+                    --stack-name $stacknamedelete
+                    shift
+                    ;;
+                *)
+                   echo "$1 is not a recognized flag!"
+                   return 1;
+                   ;;
+          esac
+  done  
 
-
-aws cloudformation create-stack --stack-name $stackname --template-body $templatepath --parameters ParameterKey=KeyPairName,ParameterValue=$keyname --capabilities CAPABILITY_NAMED_IAM --output table
-
-printf "%s\n"
-printf "running build please standby..."
-
-#sleep $sleeptime
-
-instanceid=$(aws cloudformation --region $region describe-stacks --stack-name $stackname --query "Stacks[0].Outputs[0].OutputValue")
-instanceip=$(aws cloudformation --region $region describe-stacks --stack-name $stackname --query "Stacks[0].Outputs[1].OutputValue")
-
-#aws cloudformation --region $region describe-stacks --stack-name $stackname
-
-#echo $instanceid && echo $instanceip
-
-printf "%s\n"
-printf "%s\n"
-printf "instance id is: $instanceid %s\n"
-printf "instance ip address is: $instanceip %s\n"
-printf "%s\n"
-
+#eccho out stack names when creating or deleting
+echo "Creating Stack : $stacknamecreate";
+echo "Deleting Stack : $stacknamedelete";
+  
+ 
